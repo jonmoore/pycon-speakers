fe574756539c123449ba60aac3836a5ece583c58
Gayane <pgayane@gmail.com>
2014-04-16 19:59:22 -0400

    ignor first token of the name if it is an abbriviation

--- pycon_speakers/pipelines.py
+++ pycon_speakers/pipelines.py
@@ -7,9 +7,21 @@ class GenderPipeline(object):
         self.detector = gender.Detector()
 
     def process_item(self, item, spider):
-        firstname = item['name'].split()[0]
+        firstname = self._get_firstname(item['name'])
         item['gender'] = self.detector.get_gender(firstname)
         return item
+
+
+    def _get_firstname(self, name):
+        '''
+        in case the first token contains dot (e.g. Dr., D., S., etc)
+        take the next available token
+        '''
+        name_parts = name.split() 
+        firstname = name_parts[0]
+        if firstname.find('.') != -1 and len(name_parts) > 1:
+            firstname = name_parts[1]
+        return firstname
 
 class DefaultsPipeline(object):
     """
